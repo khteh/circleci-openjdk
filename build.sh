d61d252af692881f41d57a72be64e7ebcbaf770d
Teh Kok How <kokhow.teh@4xlabs.co>
2019-10-01 17:41:20 +0800

    Change docker image repository name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #$(aws ecr get-login --no-include-email)
-docker build -t circleci .
-docker tag circleci:latest khteh/circleci:latest
-docker push khteh/circleci:latest
+docker build -t circleci-openjdk .
+docker tag circleci-openjdk:latest khteh/circleci-openjdk:latest
+docker push khteh/circleci-openjdk:latest
